3b60d1a61aac5f0224c7358ab34239d178dd9fbc
Ole Kristian Sunde <ole.sunde@me.com>
2015-01-23 13:41:40 +0100

    Update gitlivelog.sh

--- gitlivelog.sh
+++ gitlivelog.sh
@@ -7,7 +7,7 @@ do
     --all \
     --color \
     --date=short \
-    -3 \
+    -6 \
     --pretty=format:"%C(yellow)%h%x20%C(white)%cd%C(green)%d%C(reset)%x20%s%n%b%x20%C(bold)(%an)%Creset" |
     cat -
     sleep 15
