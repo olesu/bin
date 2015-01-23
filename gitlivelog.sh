7de6e9961c931477d050b76d6854f4eac92b17f2
Ole Kristian Sunde <ole.sunde@me.com>
2015-01-23 13:38:31 +0100

    Add body

--- gitlivelog.sh
+++ gitlivelog.sh
@@ -8,7 +8,7 @@ do
     --color \
     --date=short \
     -40 \
-    --pretty=format:"%C(yellow)%h%x20%C(white)%cd%C(green)%d%C(reset)%x20%s%x20%C(bold)(%an)%Creset" |
+    --pretty=format:"%C(yellow)%h%x20%C(white)%cd%C(green)%d%C(reset)%x20%s%n%b%x20%C(bold)(%an)%Creset" |
     cat -
     sleep 15
 done
